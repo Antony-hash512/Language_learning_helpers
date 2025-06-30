9701d5522a9f9e6f37b764a0f6e5a6081dbb87f8
Antony <antony.ictr@gmail.com>
2025-06-30 23:52:27 +0400

    fix for narrate

--- words_testing.sh
+++ words_testing.sh
@@ -190,6 +190,8 @@ while [[ -s "$INPUT_FILE" ]]; do
     fi
 
     sentence=$(gemini -m "$MODEL_GEMINI" -p "$PROMPT_CREATE_SENTENCE $EXTRA_PROMPT_SENTENCE" < /dev/null)
+    sentence=$(echo "$sentence" | sed 's/\*//g')
+    colored_sentence=$(echo "$sentence" | sed "s/$word/${BLUE}&${NC}${CYAN}/gi")
     add_sentence_to_db "$word" "$sentence"
 
     check_translation_function
@@ -218,7 +220,7 @@ while [[ -s "$INPUT_FILE" ]]; do
             о|o) # narrate sentence
                 echo ""
                 echo -e "${BLUE}Озвучиваю предложение:${NC}"
-                echo -e "${CYAN}$sentence${NC}"
+                echo -e "${CYAN}$colored_sentence${NC}"
                 ENCODED_TEXT=$(jq -rn --arg x "$sentence" '$x|@uri')
                 mpv "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=${ENCODED_TEXT}&tl=${LANGUAGE_INPUT_CODE}"
                 echo ""
@@ -228,7 +230,7 @@ while [[ -s "$INPUT_FILE" ]]; do
                 ;;
             р|r) # repeat sentence
                 echo ""
-                echo -e "${CYAN}$sentence${NC}"
+                echo -e "${CYAN}$colored_sentence${NC}"
                 echo ""
                 ;;
             в|q) # exit
